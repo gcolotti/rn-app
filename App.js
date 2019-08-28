166e9abbf25f3449642a1bd793f32abeef53fa57
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-08-27 22:27:45 -0300

    InputText, Add Button & FlatList: Add Goals.

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...courseGoals, {
+      key: Math.random().toString(),
+      value: enteredGoal
+    }]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder={'Course goals ...'}
+          style={styles.input}
+          onChangeText={goalInputHandler} />
+        <Button
+          title="ADD"
+          onPress={addGoalHandler} />
+      </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData =>
+          <View style={styles.listItem}>
+            <Text >{itemData.item.value}</Text>
+          </View>
+        }
+      />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', borderBottomColor: 'black',
+    borderBottomWidth: 1, padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
